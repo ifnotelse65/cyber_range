306730ee36a29676cb101f534523ac83b2ee6c11
Acolasian <101235608+Acolasian@users.noreply.github.com>
2022-04-03 04:27:39 -0400

    Add files via upload

--- docker_net/dialog.cpp
+++ docker_net/dialog.cpp
@@ -4,6 +4,10 @@
 #include<QStandardItem>
 #include<QDebug>
 #include<fstream>
+#include<cstring>
+#include<queue>
+#include<QMessageBox>
+typedef pair<int,int> node;
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
@@ -20,16 +24,7 @@ Dialog::~Dialog()
 
 void Dialog::on_buttonBox_accepted()
 {
-    QProcess *process = new QProcess();
-    //QStringList argument;
-    ofstream file("/home/seed/Desktop/cyber_range/docker_net/data.txt",ios::out|ios::trunc);
     int* matrix=new int(num*num);
-    //argument<<"D:\\Github\\cyber_range\\docker-net\\run.sh";
-    process->setWorkingDirectory("/home/seed/Desktop/cyber_range/docker_net");
-    process->start("/usr/bin/bash");
-    process->waitForStarted();
-    process->write("./run.sh \n");
-    //process->write();
     for(int i=0;i<num;i++)
     {
         for(int j=0;j<num;j++)
@@ -38,14 +33,63 @@ void Dialog::on_buttonBox_accepted()
             matrix[i*num+j]=ui->tableView->model()->data(index).toInt();
         }
     }
-    if(!file.is_open())
+    bool* vis=new bool[num];
+    bool flag=0;
+    queue<node> q;
+    for(int i=0;i<num;++i)
     {
-        qDebug()<<"fail to open this file";
+        if(flag)break;
+        while(!q.empty())q.pop();
+        memset(vis,0,sizeof(vis));
+        vis[i]=1;
+        q.push(node(-1,i));
+        while(!q.empty())
+        {
+            if(flag)break;
+            int u=q.front().second,fa=q.front().first;
+            q.pop();
+            for(int j=0;j<num;++j)
+                if(matrix[u*num+j])
+                {
+                    int v=j;
+                    if(v==fa)continue;
+                    if(vis[v])
+                    {
+                        flag=1;
+                        break;
+                    }
+                    else
+                    {
+                        vis[v]=1;
+                        q.push(node(u,v));
+                    }
+                }
+        }
     }
-    for(int i=0;i<num*num;i++)
+        delete[]vis;
+    if(flag)
+        QMessageBox::information(NULL,"error","as configuration table error",QMessageBox::Yes | QMessageBox::No,QMessageBox::Yes);
+    else
     {
-        file<<matrix[i]<<"   ";
+        QProcess *process = new QProcess();
+        //QStringList argument;
+        ofstream file("/home/seed/Desktop/cyber_range/docker_net/data.txt",ios::out|ios::trunc);
+        //argument<<"D:\\Github\\cyber_range\\docker-net\\run.sh";
+        process->setWorkingDirectory("/home/seed/Desktop/cyber_range/docker_net");
+        process->start("/usr/bin/bash");
+        process->waitForStarted();
+        process->write("./run.sh \n");
+        //process->write();
+        if(!file.is_open())
+        {
+            qDebug()<<"fail to open this file";
+        }
+        for(int i=0;i<num*num;i++)
+        {
+            file<<matrix[i]<<"   ";
+        }
     }
+
 }
 
 
